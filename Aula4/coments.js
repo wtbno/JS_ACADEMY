bd5e2f558ae64225e4180f7648463873c4bd6876
wtbno <bruno_links@hotmail.com>
2021-12-13 17:35:05 -0300

    Aulas do curso JS

---
+++ Aula4/coments.js
@@ -0,0 +1,9 @@
+//Comentários são trechos de código ignorados pelo motor da JS
+console.log('Olá mundo!');//Nenhum
+console.log('Exemplo 1');//Desses
+console.log('Exemplo 2');//Comentários
+console.log('Exemplo 3');//Serão exibidos
+
+
+//Comentários de linhas
+/*Comentários de blocos */
---
+++ Variaveis/index.js
@@ -0,0 +1,23 @@
+//Variáveis = dados salvos em um espaço na memoria do pc
+
+//Exemplo gatos
+
+let nome1 = 'kittie'; // let é mais utilizado atualmente, não pode redeclarar esse tipo de variável
+var nome2 = 'cookie'; // método de declarar com var é uma forma antiga de se declarar var//Pode ser alterado
+
+console.log('A', nome1, 'chegou em casa no ano de 2021');
+console.log('Em dezembro de 2021', nome1, 'ganhou uma fonte de presente de natal');
+
+console.log('A', nome2, 'dorme demais!!!');
+
+
+//Regras para criar variáveis
+/**
+ * Não criar var com palavras reservadas (let, console, log, var etc);
+ * As variáveis precisam ter nomes significativos e que façam sentido;
+ * Não iniciar o nome de uma var com números -> estoura um erro Invalid or unexpected token;
+ * Variáveis com traço ou espaço não são aceitas -> ex.: nome cliente ou nome-cliente
+ * -> forma correta: nomeCliente (camelCase);
+ * Variáveis são case sensitive -> nomeCliente;
+ * NÃO UTILIZAR VAR
+ */
