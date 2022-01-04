63a150f5099b05029a9646032491b9b7eb943554
wtbno <bruno_links@hotmail.com>
2022-01-04 17:41:38 -0300

    modificação de números inteiros

---
+++ aulaNumber2/index.js
@@ -0,0 +1,33 @@
+let num1 = 0.7;
+let num2 = 0.1;
+
+//console.log(num1.toString() + num2); // Ao tentar executar a saída de um numero com uma string irá ocorrer uma concatenação e não uma soma
+//num1 = num1.toString //conversão de número para string
+//console.log(typeof num1);
+//console.log(num1.toString(2));// representação binária
+//console.log(num1.toFixed(2));//selecionar casas decimais
+//.isNaN = verificar se é um número
+//.isInteger = é um número inteiro?
+//num1 = parseFloat(num1.toFixed(2))//Precisão de números
+//console.log(num1);
+
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+num1 += num2;
+console.log(num1);
+console.log(Number.isInteger(num1));
+
+num1 = parseFloat(num1.toFixed(2))//Precisão de números inteiros
+console.log(num1);
+console.log(Number.isInteger(num1));
+
