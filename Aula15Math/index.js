9be658c40d45b3b95be42deb06011c3d0a9f1f71
wtbno <bruno_links@hotmail.com>
2022-01-13 19:08:11 -0300

    Função Math

---
+++ Aula15Math/index.js
@@ -0,0 +1,12 @@
+// let num1 = 9.5698
+// let num2 = Math.floor(num1)//Arrendonda o número para baixo
+// let num2 = Math.ceil(num1)//arrendonda o número para cima
+// let num2 = Math.round(num1)//arredonda para o número mais próximo
+// console.log(num2);
+console.log(Math.max(65,154,16,6,9999,663255,15412,3122415,222222222,332));//Seleciona o maior número da seq
+console.log(Math.min(65,154,16,6,9999,663255,15412,3122415,222222222,332));//Seleciona o menor número da seq
+//const aleatorio = Math.random();
+const aleatorio = Math.random() * (10 - 100) + 7;
+console.log(aleatorio);//gera um número aleatório
+console.log(Math.PI);
+console.log(5 ** 10);//Elevação
